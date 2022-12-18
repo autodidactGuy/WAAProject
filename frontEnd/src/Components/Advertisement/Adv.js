9fdeab1745ed0c41062967f3ff93fe813306a7e5
o.benhenia <o.benhenia@gmail.com>
2022-12-18 14:08:24 -0600

    image for company

--- frontEnd/src/Components/Advertisement/Adv.js
+++ frontEnd/src/Components/Advertisement/Adv.js
@@ -16,7 +16,9 @@ const Adv = (props) => (
             style={{ margin: '5px' }}>
             <Row>
                 <Col>
-                    <Avatar shape="square" size={100} icon={<UserOutlined />} />
+                    <Avatar shape="square" size={100}
+                    src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOAAAADgCAMAAAAt85rTAAAAh1BMVEX///8ApO/yUCJ/ugD/uQD84dv/+u/+8/D/twD2iXF8uADzUBv/y00Aou//wBn/7snv+f73+u/h7snX7vzJ6fv3++ymzmXyShahzE2LwBno89dNvfNlv/MApu8ZrPD96eX/0Fv/89b4rJu924au0m3xRATu9uD8/v+G0Pfg8/3/2XttxPT/xi/dU5jdAAABYklEQVR4nO3cSVICURREUVBREaQRFcUGsMNu/+tz9qsGv4YZhnLuBjJOvPnr9SRJ0j50kq6Zml+Fq/qehuHWxfd8ne1lVQMOX5fZBgV4c5bt9qIKXB5EG7WBh9EAAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQG76/xOOcrWAkZ9HRfsrQfhmqm3Vbh5DShJ0j43S7crU8fpqr73u3Afxfc5zrbZ1oD359n6iwIcn2abTOvAfrSHNvAoGSAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICDgXwf+xnfKr8dwDXAzyfZdA+4Wl+FmZWs7DVd9EStJkv5bP2+4QdB1EE8RAAAAAElFTkSuQmCC"
+                    icon={<UserOutlined />} />
                 </Col>
                 <Col style={{marginLeft:'5px'}}>
                     <div> Campany : {props.adv.CompanyName} </div>
