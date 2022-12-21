6f17b81fb3981bdd1a3d53cccc754975594a35e7
DrOuissem <o.benhenia@gmail.com>
2022-12-21 22:17:30 +0100

    myprofile responsive

--- frontEnd/src/Components/pages/myprofile/MyProfile.js
+++ frontEnd/src/Components/pages/myprofile/MyProfile.js
@@ -2,14 +2,17 @@ import React from 'react'
 import BasicStudentInfo from '../../StudentCV/BasicStudentInfo'
 import EducationList from '../../WorkExperience/EducationList'
 import WorkExperienceList from '../../WorkExperience/WorkExperienceList'
+import { Row, Col } from 'antd';
 
 function MyProfile() {
   return (
-    <div>
+    <Row>
+      <Col xs={{ span: 23, offset: 1 }} sm={{ span: 20, offset: 4 }} md={{ span: 18, offset: 6 }}  lg={{ span: 12, offset: 6 }} xl={{ span: 12, offset: 6 }}  xxl={{ span: 12, offset: 6 }} >
       <BasicStudentInfo/>
       <WorkExperienceList/>
       <EducationList/>
-    </div>
+      </Col>
+    </Row>
   )
 }
 
