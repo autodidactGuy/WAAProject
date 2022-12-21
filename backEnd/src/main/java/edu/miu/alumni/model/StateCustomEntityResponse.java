acc761ea7fb30b93ec0a52029013b6f78d9937c1
mae-meijuan-long <meijuan.long@miu.edu>
2022-12-21 14:31:51 -0600

    add the city and state basic functions

---
+++ backEnd/src/main/java/edu/miu/alumni/model/StateCustomEntityResponse.java
@@ -0,0 +1,17 @@
+package edu.miu.alumni.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StateCustomEntityResponse {
+    private String value;
+    private String label;
+    private java.util.List<CityChild> child;
+
+
+
+}
