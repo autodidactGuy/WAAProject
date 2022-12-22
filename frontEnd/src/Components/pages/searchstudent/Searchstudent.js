c1301533db9c4ebe7570f9b50fabe6dd97afb7d2
Hassan Raza <hassanraza632@gmail.com>
2022-12-22 05:08:59 -0600

    UI Fixes

--- frontEnd/src/Components/pages/searchstudent/Searchstudent.js
+++ frontEnd/src/Components/pages/searchstudent/Searchstudent.js
@@ -5,7 +5,7 @@ import { Button, Checkbox, Form, Input, Row, Col, Spin } from 'antd';
 function searchstudent() {
   return (
     <Row>
-    <Col span={12} offset={6} className="mt-20">
+    <Col span={12} offset={1} className="mt-20 ">
   <Form
     name="search_student"
     className="search_student"
@@ -13,23 +13,20 @@ function searchstudent() {
       remember: true,
     }}
   >
+
     <Form.Item
-      name="email"
+      name="text"
     >
-      <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
-    </Form.Item>
-    <Form.Item
-      name="password"
-    >
+      <Input placeholder="text" />
       <Input
-        prefix={<LockOutlined className="site-form-item-icon" />}
-        type="password"
-        placeholder="Password"
+        type="text"
+        placeholder="text"
       />
     </Form.Item>
 
-    <Form.Item>
-      <Button type="primary" htmlType="submit" className="login-form-button">
+    <Form.Item
+    >
+      <Button type="primary" htmlType="submit" className="search-form-button">
         Search
       </Button>
     </Form.Item>
