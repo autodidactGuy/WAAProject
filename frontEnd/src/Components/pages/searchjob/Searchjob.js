2b062416f93593e07351d8b3091f0ea32d00dc99
Hassan Raza <hassanraza632@gmail.com>
2022-12-22 14:47:28 -0600

    Search Job

--- frontEnd/src/Components/pages/searchjob/Searchjob.js
+++ frontEnd/src/Components/pages/searchjob/Searchjob.js
@@ -1,11 +1,127 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import { getAccessToken } from '../../../redux/userReducer';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import Adv from '../../Advertisement/Adv';
+import { Button, Checkbox, Form, Input, Row, Col, Spin } from 'antd';
 
-function searchjob() {
-  return (
-    <div>
-      search for a job
-    </div>
-  )
+const Searchjob = () => {
+
+  const baseurl = process.env.REACT_APP_API_URL;
+
+  axios.defaults.baseURL=baseurl;
+
+  axios.defaults.headers.common["Authorization"] = "Bearer "+getAccessToken();
+
+  const [form] = Form.useForm();
+
+  const getJobs = async(searchFilters)=>{
+    if(getAccessToken()!=null){
+        setIsLoggedIn(true);
+        console.log(searchFilters);
+        const response=await axios.post("/jobAdvertisement/filterJobs",searchFilters);
+        setJobs(response.data);
+    }
+    else{
+
+    }
 }
 
-export default searchjob
+const [jobs,setJobs] = useState([]);
+
+const [isLoggedIn,setIsLoggedIn] = useState(false);
+
+useEffect(()=>{
+  
+},[]);
+
+const onFinish = (values) => {
+  const searchJobs = {};
+
+  if(values.company){
+    searchJobs.companyName=values.company;
+  }
+  if(values.state){
+    searchJobs.state=values.state;
+  }
+  if(values.city){
+    searchJobs.cityCode=values.city;
+  }
+  
+  console.log(values);
+
+  getJobs(searchJobs);
+
+  };
+
+  return (
+    <div>
+      <div className='FilterDiv'>
+      <Row className='filterForm'>
+        <Col span={18} offset={1} className="mt-20">
+
+          <Form
+            name="search_jobs"
+            className="search_jobs"
+            form={form}
+            onFinish={onFinish}
+          >
+
+          <Row>
+            <Col >
+              <Form.Item name="state">
+                <Input placeholder="State"/>
+              </Form.Item>
+
+            
+            </Col>
+            <Col>
+            <Form.Item name="city">
+                <Input placeholder="City"/>
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item name="company">
+                <Input placeholder="Company Name"/>
+              </Form.Item>
+            </Col>
+            <Col >
+              <Form.Item name="tags">
+                <Input placeholder="Tags" />
+              </Form.Item>
+            </Col>
+            <Col >
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit" className="search-form-button">
+                  Filter Jobs
+                </Button>
+              </Form.Item>
+              </Col>
+          </Row>
+        </Form>
+      </Col>
+      </Row>
+      </div>
+
+      <div className='jobs'>
+        {
+          (jobs.length>0)?
+          <Row>
+            <Col offset={1}>
+              <h1>Jobs</h1>
+              <Row >
+                  {jobs.map(job => <Col  key={job.id}  xs={24} sm={12} md={12} lg={8} xl={6}> <Adv adv={job} /> </Col>)}
+              </Row>
+            </Col>
+            </Row>
+          :
+          <>
+          </>
+        }
+      </div>
+    </div>
+    
+  )
+}
+export default Searchjob;
