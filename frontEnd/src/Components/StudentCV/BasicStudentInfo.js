2b47564007cb69a779c7ee0df08ec5cf8d98315b
DrOuissem <o.benhenia@gmail.com>
2022-12-22 23:23:03 +0100

    progress basic student info

--- frontEnd/src/Components/StudentCV/BasicStudentInfo.js
+++ frontEnd/src/Components/StudentCV/BasicStudentInfo.js
@@ -1,17 +1,21 @@
 import { MailOutlined, PhoneOutlined, UserOutlined } from '@ant-design/icons'
 import { Avatar, Button } from 'antd'
+import { useDispatch, useSelector } from "react-redux";
 import React from 'react'
 import BasicStudentInfoModal from './BasicStudentInfoModal'
+import Moment  from 'moment';
 
 
 function BasicStudentInfo() {
+  const userInfo= useSelector((state)=>state.userReducer.userInfo)
+  console.log("user in of : ",userInfo)
   return (
     <div style={{textAlign:"center"}}>
       <Avatar size={64} icon={<UserOutlined />} />
-      <div> FirstName Lastname </div>
-      <div>age : 29</div>
-      <div><PhoneOutlined />phone number : 1234 </div>
-      <div><MailOutlined />email : test@gmail.com </div>
+      <div> {userInfo.firstName}, {userInfo.lastName}  </div>
+      <div>age : {Moment().diff(userInfo.birthday, 'years')}</div>
+      <div><PhoneOutlined />phone number : {userInfo.phoneNumber} </div>
+      <div><MailOutlined />email : {userInfo.email}  </div>
       <BasicStudentInfoModal/>
 
     </div>
