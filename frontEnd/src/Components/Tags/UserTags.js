8714e0e7dbee06c350f171e9d5bf417a8af8ac97
DrOuissem <o.benhenia@gmail.com>
2022-12-22 01:14:36 +0100

    add user tags

---
+++ frontEnd/src/Components/Tags/UserTags.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { Avatar, Button, List, Select } from 'antd';
+//import { workExperienceData } from '../../Data/WorkExperienceData'
+
+import { useDispatch, useSelector } from "react-redux";
+
+const options = [];
+for (let i = 10; i < 36; i++) {
+  options.push({
+    value: i.toString(36) + i,
+    label: i.toString(36) + i,
+  });
+}
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
+
+const UserTags = () =>
+{
+    // const workExperienceData = useSelector((state) => state.jobEReducer.jobExperienceList);
+    // useEffect(() => {
+    //     dispatch(getJobExperienceList());
+    //   },[]);
+
+    // const dispatch = useDispatch();
+    
+    return (
+    
+        <div style={{ marginLeft:'5px' }}>
+            <h1> my Tags</h1>
+            
+            <Select
+                mode="tags"
+                style={{
+                width: '100%',
+                }}
+                placeholder="Tags Mode"
+                onChange={handleChange}
+                options={options}
+                
+            />
+        
+        </div>
+    );
+}
+
+export default UserTags;
