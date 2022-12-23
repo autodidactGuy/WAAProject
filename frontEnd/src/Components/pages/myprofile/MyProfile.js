37b6a6449f6161af7564906ea35f96b89bfad42d
DrOuissem <o.benhenia@gmail.com>
2022-12-22 19:06:06 -0600

    only student can have work experience, education, tags interested in

--- frontEnd/src/Components/pages/myprofile/MyProfile.js
+++ frontEnd/src/Components/pages/myprofile/MyProfile.js
@@ -4,18 +4,26 @@ import EducationList from '../../WorkExperience/EducationList'
 import WorkExperienceList from '../../WorkExperience/WorkExperienceList'
 import { Row, Col, Divider } from 'antd';
 import UserTags from './../../Tags/UserTags';
+import { useSelector } from 'react-redux';
 
 function MyProfile() {
+  const userInfo= useSelector((state)=>state.userReducer.userInfo)
+
   return (
     <Row style={{marginTop:'10px'}}>
       <Col xs={{ span: 23, offset: 1 }} sm={{ span: 20, offset: 4 }} md={{ span: 18, offset: 6 }}  lg={{ span: 12, offset: 6 }} xl={{ span: 12, offset: 6 }}  xxl={{ span: 12, offset: 6 }} >
       <BasicStudentInfo/>
-      <Divider plain  > Experience </Divider>
-      <WorkExperienceList/>
-      <Divider plain  > Education </Divider>
-      <EducationList/>
-      <Divider plain  > Tags </Divider>
-      <UserTags/>
+      
+      {
+        userInfo.role[0].name==="STUDENT" ? <><Divider plain  > Experience </Divider>
+        <WorkExperienceList/>
+        <Divider plain  > Education </Divider>
+        <EducationList/>
+        <Divider plain  > Tags </Divider>
+        <UserTags/></> : <></>
+      }
+      
+      
       </Col>
     </Row>
   )
