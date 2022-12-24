ae9f823631a86a00c4126a4399bbafd03f0ea9e6
mae-meijuan-long <meijuan.long@miu.edu>
2022-12-24 00:48:31 -0600

    job_expirence and jobAdvertisment  column change

--- backEnd/src/main/java/edu/miu/alumni/aspects/AdNotification.java
+++ backEnd/src/main/java/edu/miu/alumni/aspects/AdNotification.java
@@ -44,6 +44,7 @@ public class AdNotification {
     public void informPosterNewStuApplied(){ }
 
 
+
     @AfterReturning(
             pointcut="informPosterNewStuApplied()",
             returning="retVal")
